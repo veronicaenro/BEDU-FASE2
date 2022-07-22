5ac5e09aabfdd9d188086614244cd2304e6bf804
veronicaenro <veronicaenro@gmail.com>
2022-07-21 20:02:28 -0700

    Sesion 2 -  Reto 3 - Numeros Primos

--- S2.js
+++ S2.js
@@ -1,5 +1,5 @@
 // Ejercicio 1 sesion 2
-
+/*
 const time =1;
 let greeting;
 
@@ -14,3 +14,80 @@ if (time >= 0 && time < 12){
 }
 
 console.log(greeting);
+*/
+
+// Ejercicio 2
+
+/*
+const day = 2;
+let text;
+
+switch (day) {
+    case 1:
+        text = 'Monday';
+        break;
+    case 2:
+        text = 'Tuesday';
+        break;
+    case 3:
+        text = 'Wednesday';
+        break;
+    case 4:
+        text = 'Thursday';
+        break;
+    case 5:
+        text = 'Friday';
+        break;        
+    case 6:
+        text = 'Saturday';
+        break;
+    case 7:
+        text = 'Sunday';
+        break;                    
+    default:
+        text="Valor Inválido"
+        break;
+}
+
+console.log(text);
+*/
+
+// Ejercicio 3
+/*
+const speed = 120;
+let message;
+
+message = speed  
+
+const isFast=speed>100;
+*/
+
+// Ejemplo de Bucle
+
+//console.log('Hello world!');
+//console.log('Hello world!');
+//console.log('Hello world!');
+//console.log('Hello world!');
+//console.log('Hello world!');
+/*
+
+for (let index = 0; index < 200; index++) {
+    if(index === 101) break
+    if (index % 2 === 0) console.log('Hello world!, repeticion', index);
+}
+
+*/
+
+// Reto 3 - Numeros primos de cero a 100
+
+for (let counter = 0; counter <= 100; counter ++) {
+    let isPrime=true;
+
+    for(let index = 2; index <= counter; index++){
+        if (counter % index === 0 && index !== counter){
+            isPrime=false;
+        }
+    }
+    
+    if(isPrime) console.log('Numero Primo es', counter);
+}
