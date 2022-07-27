98d32540b8178fae884651c7a4796826813387d7
veronicaenro <veronicaenro@gmail.com>
2022-07-26 17:48:25 -0700

    Sesion 3 - Ejercicio 1 - For of

--- S3.js
+++ S3.js
@@ -1,5 +1,5 @@
 // Sesion 3 - Ejemplo 1
-
+/*
 const numbers = [1,3,4,7,2,1,9,0]
 
 // const doubled = [2,6,8,14,4,2,18,0]   <- Resultado
@@ -11,5 +11,18 @@ for (let index=0; index < numbers.length; index++){
     doubled.push(numbers[index] * 2)
 }
 
-console.log(numbers);
-console.log(doubled);
+console.log('Numbers:', numbers);
+console.log('Doubled:', doubled);
+*/
+
+
+const numbers = [1,3,4,7,2,1,9,0]
+const doubled = []
+
+for (const number of numbers) {
+    doubled.push(number * 2)
+}
+
+
+console.log('Numbers:', numbers);
+console.log('Doubled:', doubled);
