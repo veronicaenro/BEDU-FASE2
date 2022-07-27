86489ac7c31ff69b3c429b27cad5a86c40a4980c
veronicaenro <veronicaenro@gmail.com>
2022-07-26 18:11:09 -0700

    Sesion 3 - Reto 1

--- S3.js
+++ S3.js
@@ -28,7 +28,7 @@ console.log('Numbers:', numbers);
 console.log('Doubled:', doubled);
 */
 
-/*------------------------------------ */
+/*------------------------------------ 
 const numbers = [1,3,4,7,2,1,9,0]
 const doubled = []
 
@@ -40,3 +40,16 @@ for (const number of numbers) {
 
 console.log('Numbers:', numbers);
 console.log('Doubled:', doubled);
+*/
+
+// Reto 1 - Calcular promedio
+
+const numbers = [5,3,4,7,2,1,9,7,7];
+let suma = 0;
+
+for (const number of numbers) {
+    suma+=number;
+}
+
+const promedio=suma/(numbers.length);
+console.log('Promedio:', promedio);
