857ac478e63ecc9824ba70ce09af2deba5d7c000
veronicaenro <veronicaenro@gmail.com>
2022-07-28 17:52:09 -0700

    Sesion 4 - Ejemplo 2

--- S4.js
+++ S4.js
@@ -11,9 +11,19 @@ function calculateAge(birthYear){
     const age = 2022 - birthYear;
     return age;
 }
-
+/*
 const ageJohn = calculateAge(1969);
 
 console.log('La edad de John es: ', ageJohn) // 53
+*/
 
 
+// Ejemplo 2 - Calcular los años que faltan para el retiro
+
+function yearsUntilRetirement(yearOfBirth, name){
+    const age = calculateAge(yearOfBirth);
+    const retirement = 65 - age;
+    console.log(`${name} retires in ${retirement} years`);
+}
+
+yearsUntilRetirement(1969, 'Jose');
