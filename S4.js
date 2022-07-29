aeef32ae860d328610b23ec118a20567c461c8ff
veronicaenro <veronicaenro@gmail.com>
2022-07-28 17:47:14 -0700

    Sesion 4 - Ejemplo 1

---
+++ S4.js
@@ -0,0 +1,19 @@
+// Sesion 4 - Funciones
+/*
+function myFunction(parameter1, parameter2){
+    //ejecutar algun codigo
+}
+*/
+
+// Ejemplo 1 - Calcular Edad
+
+function calculateAge(birthYear){
+    const age = 2022 - birthYear;
+    return age;
+}
+
+const ageJohn = calculateAge(1969);
+
+console.log('La edad de John es: ', ageJohn) // 53
+
+
