f8789ef5676b1757e22d6d97442128dfb508eaac
veronicaenro <veronicaenro@gmail.com>
2022-08-02 17:49:31 -0700

    Sesion 5 - Ejemplos de Scope de Variables

---
+++ S5.js
@@ -0,0 +1,22 @@
+// Sesion 5 
+/*
+if (true){
+    var foo = 'John Doe'
+    console.log(foo)
+}
+
+console.log(foo)
+
+*/
+
+var numbers = [1,2,3,4,5]
+var doubles = []
+
+for (let i=0;i<numbers.length;i++){
+    doubles.push(numbers[i]*2)
+}
+
+console.log(numbers)
+console.log(doubles)
+//console.log(i)
+
