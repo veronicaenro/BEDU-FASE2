10eb058642d9378b339f7ba395589cfcc03b0cfd
veronicaenro <veronicaenro@gmail.com>
2022-08-02 19:18:26 -0700

    Sesion 5 - Reto 1 - Pluck

--- S5.js
+++ S5.js
@@ -71,7 +71,7 @@ const power = (base,exponent) => {
 */
 
 // Ejemplo de arrow function y THIS
-
+/*
 const john = {
     firstName:'John',
     lastName:'Doe',
@@ -86,4 +86,34 @@ const john = {
 john.calculateAge()
 
 console.log(john)
+*/
+
+// Reto 1 
+
+var singers = [
+    { name: 'Steven Tyler', band: 'Aerosmith', born: 1948 },
+    { name: 'Karen Carpenter', band: 'The Carpenters', born: 1950 },
+    { name: 'Kurt Cobain', band: 'Nirvana', born: 1967 },
+    { name: 'Chris Cornell', band: 'Soundgarden', born: 1964 },
+  ];
+  
+function pluck(list, propertyName) {
+    let values = [];
+
+    for(let i = 0 ; i < list.length ; i++){
+        values.push(list[i][propertyName])
+    }
+    return values
+}
+
+  
+  console.log( pluck(singers, 'name') );
+  // ["Steven Tyler", "Karen Carpenter", "Kurt Cobain", "Chris Cornell"]
+  
+  console.log( pluck(singers, 'band') );
+  // ["Aerosmith", "The Carpenters", "Nirvana", "Soundgarden"]
+  
+  console.log( pluck(singers, 'born') );
+  // [1948, 1950, 1967, 1964]
+
 
