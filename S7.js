bdae7839a2a1911ebec7e8c0e5ad2e2c1875d822
veronicaenro <veronicaenro@gmail.com>
2022-08-09 17:59:50 -0700

    Sesion 7: Programacion imperativa y funcional - Ejemolo 1

---
+++ S7.js
@@ -0,0 +1,70 @@
+// Ejemplo de Programacion IMPERATIVA - Decir que es lo que tiene que hacer
+
+/*
+const numbers = [1,2,3,4,5];
+const doubles = [];
+
+for (var i = 0; i < numbers.length; i++){
+    doubles.push(numbers[i]*2);
+}
+
+console.log (numbers);
+console.log (doubles);
+*/
+
+//Programacion funcional - Se enfoca en lo que se quiere conseguir
+
+/*
+const numbers = [1,2,3,4,5];
+const doubles = numbers.map(function(number){ // Map va por cada uno de los datos 
+    return number * 2;
+});
+
+console.log (numbers);
+console.log (doubles);
+*/
+
+// Ejemplo 1
+
+const car = {
+    brand: 'Nissan',
+    model: 'Sentra',
+    year: 2020
+}
+ //funcion que muta los datos
+ /*
+function addColor(car){
+    car.color = 'Black';
+    return car;
+}
+*/
+// funcion que crea un nuevo objeto
+
+/*
+function addColor(car){
+    const newCar = Object.assign ({}, car, {
+        color:'Black'
+    });
+    return newCar;
+}
+*/
+
+//Spread Operator
+
+function addColor(car){
+    const newCar ={
+        ...car,
+        color:'Black'
+    }
+    return newCar;
+}
+
+console.log('Antes de ejecutar la funcion: ', car);
+
+const sameCar = addColor(car)
+
+console.log('Despues de ejecutar la funcion: ', car);
+console.log('Despues de ejecutar la funcion: ', sameCar);
+
+console.log('Son los mismos autos? ', car === sameCar)
+
